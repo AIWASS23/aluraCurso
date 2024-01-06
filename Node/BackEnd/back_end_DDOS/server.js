643b7503eaa4f52d0782b8641fb1d7008dc608db
AIWASS23 <marcelodearaujo677@gmail.com>
2024-01-06 15:54:12 -0300

    estudo de backEnd para o login

--- Node/BackEnd/back_end_DDOS/server.js
+++
@@ -1,5 +0,0 @@
-import app from './src/app.js';
-
-app.listen(8080, () => {
-    console.log("Initializing port 8080");
-});
--- Node/BackEnd/back_end_DDOS/src/routes/router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import Controller from "../controllers/controller.js"
-const router = Router();
-const controller = new Controller();
-router.post('/login', controller.login);
-router.get('/users', controller.getAllUsers);
-
-export default router;
